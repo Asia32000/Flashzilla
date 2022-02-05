d8b4946c3deb2b99ec6389e8ebdb0cffb7bcfd96
Joanna <astalenczyk2@wp.pl>
2022-02-05 13:51:48 +0100

    First version

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Joanna Staleńczyk on 04/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
